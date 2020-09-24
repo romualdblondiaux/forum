282b92b2d7ef8a0efc9ec34b679be822b10ba4b2
romualdblondiaux <romuald.blondiaux@gmail.com>
2020-09-24 10:02:16 +0200

    creation formulaire inscription

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
-<?php 
+<?php
     session_start();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>document</title>
+    <title>Document</title>
 </head>
 <body>
     <form action="index.php" method="POST">
         <div>
-            <label for="login">Login :</label>
+            <label for="login">Login: </label>
             <input type="text" id="login" name="login">
         </div>
         <div>
-            <label for="password">Password :</label>
+            <label for="password">Mot de passe</label>
             <input type="password" id="password" name="password">
         </div>
         <div>
-            <input type="submit" value="connexion">
-        </div>  
-    </form>
-        <div>
-            <a href="inscription.php">Pas encore inscrit?</a>
+            <input type="submit" value="Connexion">
         </div>
+    </form>
+    <div>
+        <a href="inscription.php">Pas encore inscrit?</a>
+    </div>
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>inscription</h1>  
+    <form action="treatmentRegister.php" method="POST">
+        <div>
+            <label for="login">Login: </label>
+            <input type="text" id="login" name="login" placeholder="Votre login">
+        </div>
+        <div>
+            <label for="email">Email: </label>
+            <input type="email" id="email" name="email" placeholder="Votre adresse e-mail">
+        </div>
+        <div>
+            <label for="pass1">Mot de passe</label>
+            <input type="password" id="pass1" name="password">
+        </div>
+        <div>
+            <input type="submit" value="Inscription">
+        </div>
+    </form> 
 </body>
 </html>
