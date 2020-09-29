8f55017bd2dbf670b966ac19376e08e267c59b50
romualdblondiaux <romuald.blondiaux@gmail.com>
2020-09-29 12:35:32 +0200

    admin:

--- admin/index.php
+++ admin/index.php
@@ -19,9 +19,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Administration</title>
 </head>
 <body>
-    <h1>Administration</h1>
+    <h1>Gestion Administration</h1>
+        <a href="membre.php">liste des membres</a>
 </body>
 </html>
