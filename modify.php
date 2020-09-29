7dd00c93543d76ec3c5e0eb3c480cd3c9d4bfb73
romualdblondiaux <romuald.blondiaux@gmail.com>
2020-09-29 11:11:51 +0200

    modify:

--- modify.php
+++ modify.php
@@ -1,9 +1,10 @@
 <?php
+    session_start();
     if(!isset($_SESSION['id'])){
         header("LOCATION:index.php");
     }
 
-    if(!isset($_GET['id'])){
+    if(isset($_GET['id'])){
         $id=htmlspecialchars($_GET['id']);
     }else{
         header("LOCATION:index.php");
@@ -21,16 +22,39 @@
         header("LOCATION:index.php");
     }
 
+    if(isset($_POST['message'])){
+        if(!empty($_POST['message'])){
+            $message=htmlspecialchars($_POST['message']);
+            $update = $bdd->prepare("UPDATE post SET message=:mess WHERE id=:myid");
+            $update->execute([
+                ":mess"=>$message,
+                ":myid"=>$id
+            ]);
+            $update->closeCursor();
+            header("LOCATION:index.php?update=succes");
+        }else{
+            header("LOCATION:modify.php?id=".$id); 
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Modifier</h1>
+    <form action="modify.php?id=<?= $donPost['id'] ?>" method="POST">
+        <div class="form-group">
+            <textarea name="message" id="message" cols="30" rows="10"><?= $donPost['message'] ?></textarea>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="modifier">
+        </div>
+    </form>
 </body>
 </html>
